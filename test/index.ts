1ba81708ed49f6ee7da2b90d86cdea1a38414dcd
corno <github.a@schraverus.com>
2020-02-18 22:18:25 +0100

    made tests generic

--- test/index.ts
+++ test/index.ts
@@ -3,31 +3,42 @@
 */
 
 import * as assert from "assert"
-import * as bc from "bass-clarinet"
+import * as chai from "chai"
 import * as fs from "fs"
+import * as path from "path"
 import { describe } from "mocha"
 import { validateDocument } from "../src"
 
 const schemasDir = "./test/tests"
 
-describe("main", () => {
-    it("foo", () => {
-        const filePath = "./test/tests/foo.x"
+type Issue = [string, "warning" | "error", number, number, number, number]
 
-        const instance = fs.readFileSync(filePath, { encoding: "utf-8" })
+type Issues = Issue[]
+
+describe("main", () => {
+    fs.readdirSync(schemasDir).forEach(dir => {
+        const testDirPath = path.join(schemasDir, dir)
+        const filePath = path.join(testDirPath, "data.x")
+        const expectedIssues = JSON.parse(fs.readFileSync(path.join(testDirPath, "issues.json"), { encoding: "utf-8" }))
+
+        const actualIssues: Issues = []
+
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" })
         validateDocument(
-            instance,
+            data,
             schemasDir,
             (errorMessage, range) => {
-                console.error(`${filePath}${bc.printRange(range)} error: ${errorMessage}`)
-                //assert.fail(`error: ${errorMessage} @ ${bc.printLocation(location)}`)
+                actualIssues.push([errorMessage, "error", range.start.line, range.start.column, range.end.line, range.end.column])
             },
             (warningMessage, range) => {
-                console.error(`${filePath}${bc.printRange(range)} warning: ${warningMessage}`)
-                //assert.fail(`warning: ${warningMessage} @ ${bc.printLocation(location)}`)
+                actualIssues.push([warningMessage, "warning", range.start.line, range.start.column, range.end.line, range.end.column])
 
             }
         )
-        assert.ok(true, "DUMMY")
+
+        it(dir, () => {
+            chai.assert.deepEqual(actualIssues, expectedIssues)
+            assert.ok(true, "DUMMY")
+        })
     })
 })
---
+++ test/tests/internal schema/data.x
@@ -0,0 +1,27 @@
+! (
+    'component types': {
+        'root': (
+            'node': (
+                'properties': {
+                    'a1': (
+                        'type': | 'value' (
+                            'type': | 'number' (
+                            )
+                        )
+                    )
+                    'b': (
+                        'type': | 'value' (
+                            'type': | 'number' (
+                            )
+                        )
+                    )
+                }
+            )
+        )
+    }
+    'root type': 'root'
+)
+{
+    "a": "B"
+    "b": "X"
+}
