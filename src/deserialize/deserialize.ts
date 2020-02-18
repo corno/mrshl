d474fc9f6c2c6c362b749fe7ecfadacddf7ca9a3
corno <github.a@schraverus.com>
2020-02-18 09:20:49 +0100

    allow both compact and non-compact

--- src/deserialize/deserialize.ts
+++ src/deserialize/deserialize.ts
@@ -6,101 +6,120 @@ function assertUnreachable<RT>(_x: never): RT {
     throw new Error("Unreachable")
 }
 
-function createNodeDeserializer(context: bc.ErrorContext, nodeDefinition: md.Node, nodeBuilder: NodeBuilder, isCompact: boolean): bc.ValueHandler {
-    const expectedElements: bc.ValueHandler[] = []
-    nodeDefinition.properties.forEach((propDefinition, propKey) => {
-        expectedElements.push(((): bc.ValueHandler => {
-            switch (propDefinition.type[0]) {
-                case "collection": {
-                    const $ = propDefinition.type[1]
-                    const collBuilder = nodeBuilder.setCollection(propKey)
-                    switch ($.type[0]) {
-                        case "dictionary": {
-                            const $$ = $.type[1]
-                            switch ($$["has instances"][0]) {
-                                case "no": {
-                                    return context.expectDictionary(_key => {
-                                        throw new Error(`unexpected entries @ ...`)
-                                    })
-                                }
-                                case "yes": {
-                                    const $$$ = $$["has instances"][1]
-                                    return context.expectDictionary(_key => {
-                                        const entry = collBuilder.createEntry()
-                                        entry.insert() //might be problematic.. insertion before fully initialized
-                                        return createNodeDeserializer(context, $$$.node, entry.node, isCompact)
-                                    })
-                                }
-                                default:
-                                    return assertUnreachable($$["has instances"][0])
-                            }
+function createPropertyDeserializer(context: bc.ErrorContext, propDefinition: md.Property, propKey: string, nodeBuilder: NodeBuilder, isCompact: boolean): bc.ValueHandler {
+    switch (propDefinition.type[0]) {
+        case "collection": {
+            const $ = propDefinition.type[1]
+            const collBuilder = nodeBuilder.setCollection(propKey)
+            switch ($.type[0]) {
+                case "dictionary": {
+                    const $$ = $.type[1]
+                    switch ($$["has instances"][0]) {
+                        case "no": {
+                            return context.expectDictionary(_key => {
+                                throw new Error(`unexpected entries @ ...`)
+                            })
                         }
-                        case "list": {
-                            const $$ = $.type[1]
-
-                            switch ($$["has instances"][0]) {
-                                case "no": {
-                                    return context.expectDictionary(_key => {
-                                        throw new Error(`unexpected entries @ ...`)
-                                    })
-                                }
-                                case "yes": {
-                                    const $$$ = $$["has instances"][1]
-                                    return context.expectDictionary(_key => {
-                                        const entry = collBuilder.createEntry()
-                                        entry.insert()
-                                        return createNodeDeserializer(context, $$$.node, entry.node, isCompact)
-                                    })
-                                }
-                                default:
-                                    return assertUnreachable($$["has instances"][0])
-                            }
+                        case "yes": {
+                            const $$$ = $$["has instances"][1]
+                            return context.expectDictionary(_key => {
+                                const entry = collBuilder.createEntry()
+                                entry.insert() //might be problematic.. insertion before fully initialized
+                                return createNodeDeserializer(context, $$$.node, entry.node, isCompact)
+                            })
                         }
                         default:
-                            return assertUnreachable($.type[0])
+                            return assertUnreachable($$["has instances"][0])
                     }
                 }
-                case "component": {
-                    const $ = propDefinition.type[1]
-                    const componentBuilder = nodeBuilder.setComponent(propKey)
-                    return createNodeDeserializer(context, $.type.get().node, componentBuilder.node, isCompact)
-                }
-                case "state group": {
-                    const $ = propDefinition.type[1]
-                    return context.expectTaggedUnionOrArraySurrogate(stateName => {
-                        const stateDef = $.states.get(stateName) //FIX handle missing state more elegantly
-                        const state = nodeBuilder.setStateGroup(propKey, stateName)
-                        return createNodeDeserializer(context, stateDef.node, state.node, isCompact)
-                    })
-                }
-                case "value": {
+                case "list": {
+                    const $$ = $.type[1]
 
-                    const $ = propDefinition.type[1]
-                    switch ($.type[0]) {
-                        case "number": {
-                            return context.expectNumber(value => nodeBuilder.setNumber(propKey, value))
+                    switch ($$["has instances"][0]) {
+                        case "no": {
+                            return context.expectDictionary(_key => {
+                                throw new Error(`unexpected entries @ ...`)
+                            })
                         }
-                        case "boolean": {
-                            return context.expectBoolean(value => nodeBuilder.setBoolean(propKey, value))
-                        }
-                        case "string": {
-                            return context.expectString(value => nodeBuilder.setString(propKey, value))
+                        case "yes": {
+                            const $$$ = $$["has instances"][1]
+                            return context.expectDictionary(_key => {
+                                const entry = collBuilder.createEntry()
+                                entry.insert()
+                                return createNodeDeserializer(context, $$$.node, entry.node, isCompact)
+                            })
                         }
                         default:
-                            return assertUnreachable($.type[0])
+                            return assertUnreachable($$["has instances"][0])
                     }
                 }
                 default:
-                    return assertUnreachable(propDefinition.type[0])
+                    return assertUnreachable($.type[0])
             }
-        })())
-    })
-    return context.expectArrayType(
-        expectedElements,
-        () => {
-            //
         }
-    )
+        case "component": {
+            const $ = propDefinition.type[1]
+            const componentBuilder = nodeBuilder.setComponent(propKey)
+            return createNodeDeserializer(context, $.type.get().node, componentBuilder.node, isCompact)
+        }
+        case "state group": {
+            const $ = propDefinition.type[1]
+            return context.expectTaggedUnionOrArraySurrogate(stateName => {
+                const stateDef = $.states.get(stateName) //FIX handle missing state more elegantly
+                const state = nodeBuilder.setStateGroup(propKey, stateName)
+                return createNodeDeserializer(context, stateDef.node, state.node, isCompact)
+            })
+        }
+        case "value": {
+
+            const $ = propDefinition.type[1]
+            switch ($.type[0]) {
+                case "number": {
+                    return context.expectNumber(value => nodeBuilder.setNumber(propKey, value))
+                }
+                case "boolean": {
+                    return context.expectBoolean(value => nodeBuilder.setBoolean(propKey, value))
+                }
+                case "string": {
+                    return context.expectString(value => nodeBuilder.setString(propKey, value))
+                }
+                default:
+                    return assertUnreachable($.type[0])
+            }
+        }
+        default:
+            return assertUnreachable(propDefinition.type[0])
+    }
+}
+
+function createNodeDeserializer(context: bc.ErrorContext, nodeDefinition: md.Node, nodeBuilder: NodeBuilder, isCompact: boolean): bc.ValueHandler {
+    if (isCompact) {
+        const expectedElements: bc.ValueHandler[] = []
+        nodeDefinition.properties.forEach((propDefinition, propKey) => {
+            expectedElements.push(((): bc.ValueHandler => {
+                return createPropertyDeserializer(context, propDefinition, propKey, nodeBuilder, isCompact)
+            })())
+        })
+        return context.expectArrayType(
+            expectedElements,
+            () => {
+                //nothing to do on end
+            }
+        )
+
+    } else {
+        const expectedEntries: { [key: string]: bc.ValueHandler } = {}
+        nodeDefinition.properties.forEach((propDefinition, propKey) => {
+            expectedEntries[propKey] = createPropertyDeserializer(context, propDefinition, propKey, nodeBuilder, isCompact)
+        })
+        return context.expectType(
+            expectedEntries,
+            () => {
+                //nothing to do on end
+            }
+        )
+
+    }
 
 }
 
