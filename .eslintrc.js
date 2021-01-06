47f6850ae2315a9110bf7a384739a78edf1b0788
corno <gitkraken.a@schraverus.com>
2021-01-06 12:13:07 +0100

    fixed linter: 'no-shadow'

--- .eslintrc.js
+++ .eslintrc.js
@@ -129,13 +129,7 @@ module.exports = {
         "no-invalid-this": "error",
         "no-multiple-empty-lines": "error",
         "no-new-wrappers": "error",
-        "no-shadow": [
-            "error",
-            {
-                "allow": ["$", "cp", "p"],
-                "hoist": "all"
-            }
-        ],
+        "no-shadow": "off",
         "no-throw-literal": "error",
         "no-trailing-spaces": "error",
         "no-undef-init": "error",
